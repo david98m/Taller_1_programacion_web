68e97f4bd02c7cb6cee9de60252b57c667fb55d9
david98m <damc91@gmail.com>
2025-07-19 14:59:22 -0500

    Add files via upload

---
+++ Ejercicios taller  1/script.js
@@ -0,0 +1,81 @@
+/*EJERCICIO 1 */
+
+// let numero_uno = prompt("ingrese el primer numero: ")
+// let numero_dos = prompt("ingrese el segundo numero: ")
+// let numero_tres = prompt("ingrese el tercer numero: ")
+
+// console.log(typeof numero_uno)
+// console.log(typeof numero_dos)
+// console.log(typeof numero_tres)
+
+/*EJERCICIO 2*/
+
+// let numero = prompt("Ingrese un numero: ")
+// if(isNaN(numero)){
+//     console.log("No es un numero")
+// }else{
+//     console.log("Si es un numero")
+// }
+
+/*EJERCICIO 3*/
+
+// let numero_decimal = prompt("Ingrese un numero decimal: ")
+// console.log(parseInt(numero_decimal))
+// console.log(parseFloat(numero_decimal))
+// console.log(Number(numero_decimal))
+
+/*EJERCICIO 4*/
+
+// var Nombre = prompt("Ingrese su nombre: ");
+// const cedula = prompt("Ingrese su cedula: ");
+// let edad = prompt("Ingrese su edad: ")
+// console.log(Nombre);
+// console.log(cedula);
+// console.log(edad);
+
+/*EJERCICIO 5*/
+
+// let numero_uno = Number(prompt("Ingrese el primer numero: "));
+// let numero_dos = Number(prompt("Ingrese el segundo numero: "));
+// console.log(numero_uno + numero_dos);
+
+/*EJERCICIO 6*/
+
+// let nombre = prompt("Ingrese su nombre");
+// let edad = prompt("Ingrese su edad: ");
+// let ciudad = prompt("Ingrese su ciudad: ");
+
+// console.log("Su nombre es: ", nombre, " y tiene un tipo de dato: " + typeof nombre);
+
+// if (isNaN(edad)) {
+//     console.log("No es un numero");
+// } else {
+//     console.log("Su edad es: ", edad, " y tiene un tipo de dato: " + typeof edad);
+// }
+
+// console.log("Su ciudad es: ", ciudad, " y tiene un tipo de dato: " + typeof ciudad);
+
+
+/*EJERCICIO 7*/
+
+// let dato = prompt("Ingrese un dato");
+
+// if (isNaN(dato)) {
+//     console.log("No es un numero valido", " y tiene un tipo de dato: "  + typeof dato);
+// } else {
+//     console.log("Si es un numero valido", " y tiene un tipo de dato: " + typeof dato);
+// }
+
+/*EJERCICIO 9*/
+
+let edad = prompt("Ingrese su edad: ");
+if (isNaN(edad)) {
+    console.log("No es un numero valido");
+} else if (isNaNf(parseInt(edad < 18))){
+        console.log("No esta autorizado para votar, no cumples con la edad necesaria")
+    }else{
+        console.log("Estas autorizado para votar, puedes pasar a tu mesa asignada")
+    }
+
+
+/*EJERCICIO 10*/
